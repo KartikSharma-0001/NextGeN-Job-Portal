093c758d1f50aa1fb2293b9efaeb034e76e55d2a
KartikSharma-0001 <135609200+KartikSharma-0001@users.noreply.github.com>
2025-07-05 18:46:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import path from 'path'
 import mongoose from 'mongoose'
-// import {Jobseeker_reg} from '../models/Jobseeker_reg.js'
+
 
 
 
@@ -9,26 +9,17 @@ const app= express();
 app.use(express.urlencoded({extended:true}));
 
 
-mongoose.connect("mongodb+srv://kartiksharma4449:B2MFB9fG14xaQwYw@cluster1.yinf0vn.mongodb.net/", {
+mongoose.connect(process.env.MONGO_URL , {
     dbName:"NextGeN_Job_Portal"
 }).then(()=>console.log("MongoDb connected...!")).catch((err)=> console.log(err))
 
-// app.get('/',(req,res)=>{
 
-//     const dir=path.resolve();
-
-//     res.sendFile(path.join(backend,'login.html'));
-    
-// });
 
 app.get('/' ,(req,res)=>{
     res.render('login.ejs')
 })
 
-// app.post('/logged-in'), (req,res)=>{
-//     res.json(req.body)
-// }
 
 
-const port= 2000;
+const port= process.env.PORT;
 app.listen(port,()=> console.log(`Server is running on ${port}`))
